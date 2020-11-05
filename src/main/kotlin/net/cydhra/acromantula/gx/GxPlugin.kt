ed40fac779cbc0945d1b6515228d0d0baf8679e2
Cydhra <ubezl@student.kit.edu>
2020-11-05 18:46:37 +0100

    register GxPlugin and strategies

--- src/main/kotlin/net/cydhra/acromantula/gx/GxPlugin.kt
+++ src/main/kotlin/net/cydhra/acromantula/gx/GxPlugin.kt
@@ -1,14 +1,24 @@
 package net.cydhra.acromantula.gx
 
+import net.cydhra.acromantula.features.importer.ImporterFeature
+import net.cydhra.acromantula.features.view.GenerateViewFeature
 import net.cydhra.acromantula.plugins.AcromantulaPlugin
+import org.apache.logging.log4j.LogManager
 
 class GxPlugin : AcromantulaPlugin {
+
+    companion object {
+        private val logger = LogManager.getLogger()
+    }
+
     override val author: String = "Cydhra"
 
     override val name: String = "CT3 GX Parsers"
 
     override fun initialize() {
-
+        ImporterFeature.registerImporterStrategy(ArtArchiveImporterStrategy())
+        GenerateViewFeature.registerViewGenerator(GxtxViewGenerator())
+        logger.info("registered gx parsers")
     }
 
 }
