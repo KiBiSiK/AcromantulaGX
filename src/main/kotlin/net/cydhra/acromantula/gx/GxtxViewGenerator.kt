53ca4d337df8b10977fe239dd4e9d88d93664cf4
Cydhra <ubezl@student.kit.edu>
2020-11-19 16:57:04 +0100

    cleaned up gxtx view generator, flip image

--- src/main/kotlin/net/cydhra/acromantula/gx/GxtxViewGenerator.kt
+++ src/main/kotlin/net/cydhra/acromantula/gx/GxtxViewGenerator.kt
@@ -6,11 +6,18 @@ import net.cydhra.acromantula.gx.util.ByteBufferedImage
 import net.cydhra.acromantula.workspace.WorkspaceService
 import net.cydhra.acromantula.workspace.disassembly.FileRepresentation
 import net.cydhra.acromantula.workspace.filesystem.FileEntity
+import net.cydhra.acromantula.workspace.filesystem.FileType
+import net.cydhra.acromantula.workspace.filesystem.pngFileType
 import org.apache.logging.log4j.LogManager
+import java.awt.geom.AffineTransform
+import java.awt.image.AffineTransformOp
+import java.awt.image.BufferedImage
 import java.io.ByteArrayOutputStream
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 import javax.imageio.ImageIO
+import kotlin.experimental.and
+import kotlin.math.pow
 
 /**
  * Generate a PNG from a GT-Texture so the user can view and/or export it.
@@ -64,29 +71,88 @@ class GxtxViewGenerator : ViewGeneratorStrategy {
                     "compression: $compressionType]"
         )
 
-        return when (compressionType) {
-            GxtxCompressionType.DXT1 -> {
-                val decompressedData =
-                    Squish.decompressImage(null, width, height, dataBuffer, Squish.CompressionType.DXT1)
-                val img = ByteBufferedImage(width, height, decompressedData)
-                val byteArrayOutputStream = ByteArrayOutputStream()
-                ImageIO.write(img, "png", byteArrayOutputStream)
+        val img = convertDataToImage(dataBuffer, width, height, compressionType)
+        val byteArrayOutputStream = ByteArrayOutputStream()
 
-                WorkspaceService.addFileRepresentation(fileEntity, this.viewType, byteArrayOutputStream.toByteArray())
+        // images are stored up-side down, therefore flip them before storing
+        val flipTransform = AffineTransformOp(
+            AffineTransform.getScaleInstance(1.0, -1.0).also { it.translate(0.0, (-height).toDouble()) },
+            AffineTransformOp.TYPE_NEAREST_NEIGHBOR
+        )
+
+        // write image as png into file system
+        ImageIO.write(flipTransform.filter(img, null), "png", byteArrayOutputStream)
+        return WorkspaceService.addFileRepresentation(fileEntity, this.viewType, byteArrayOutputStream.toByteArray())
+    }
+
+    /**
+     * Convert a byte array into a standard [BufferedImage] depending on the given [encoding]. No additional operations
+     * besides decoding and/or decompression are performed upon the data.
+     *
+     * @param data the byte array read from the gxtx resource
+     * @param width image width read in header
+     * @param height image height read in header
+     * @param encoding [GxtxCompressionType] read in header
+     */
+    private fun convertDataToImage(
+        data: ByteArray,
+        width: Int,
+        height: Int,
+        encoding: GxtxCompressionType
+    ): BufferedImage {
+        return when (encoding) {
+            GxtxCompressionType.DXT1 -> {
+                ByteBufferedImage(
+                    width,
+                    height,
+                    Squish.decompressImage(null, width, height, data, Squish.CompressionType.DXT1)
+                )
             }
             GxtxCompressionType.DXT3 -> {
-                val decompressedData =
-                    Squish.decompressImage(null, width, height, dataBuffer, Squish.CompressionType.DXT3)
-                val img = ByteBufferedImage(width, height, decompressedData)
-                val byteArrayOutputStream = ByteArrayOutputStream()
-                ImageIO.write(img, "png", byteArrayOutputStream)
+                ByteBufferedImage(
+                    width,
+                    height,
+                    Squish.decompressImage(null, width, height, data, Squish.CompressionType.DXT3)
+                )
+            }
+            GxtxCompressionType.A4R4G4B4 -> {
+                val img = BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB)
+                val imageBuffer = img.raster.dataBuffer
+                var i = 0
+                // TODO: warning, this code does not produce sane results. The actual encoding in the resources is not simple ARGB4
+                while (i < data.size) {
+                    val alpha = ((data[i] and 0xF0.toByte()).toInt() shr 4) * 2.0.pow(4.0).toInt()
+                    val red = (data[i] and 0x0F.toByte()).toInt() * 2.0.pow(4.0).toInt()
+                    val green = ((data[i + 1] and 0xF0.toByte()).toInt() shr 4) * 2.0.pow(4.0).toInt()
+                    val blue = (data[i + 1] and 0x0F.toByte()).toInt() * 2.0.pow(4.0).toInt()
+                    val argb = (alpha shl 24) or (red shl 16) or (green shl 8) or (blue)
+                    imageBuffer.setElem(i / 2, argb)
+                    i += 2
+                }
 
-                WorkspaceService.addFileRepresentation(fileEntity, this.viewType, byteArrayOutputStream.toByteArray())
+                img
+            }
+            GxtxCompressionType.A8R8G8B8 -> {
+                val img = BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB)
+                val imageBuffer = img.raster.dataBuffer
+                var i = 0
+                // TODO: warning, this code does not produce sane results. The actual encoding in the resources is not simple ARGB4
+                while (i < data.size) {
+                    val alpha = data[i].toInt()
+                    val red = data[i + 1].toInt()
+                    val green = data[i + 2].toInt()
+                    val blue = data[i + 3].toInt()
+                    val argb = (alpha shl 24) or (red shl 16) or (green shl 8) or (blue)
+                    imageBuffer.setElem(i, argb)
+                    i += 4
+                }
+
+                img
             }
             // TODO decompress remaining compression formats
             else -> {
                 logger.warn("decompression failed, decompression algorithm unsupported.")
-                WorkspaceService.addFileRepresentation(fileEntity, this.viewType, dataBuffer)
+                throw java.lang.IllegalArgumentException("encoding $encoding is not implemented")
             }
         }
     }
